81dca1117e4a7f2f5483668d3f4ecfce48548ac1
Jhon Alessandro <astralgmd@gmail.com>
2024-08-01 15:26:16 -0400

    style(itemBody): set style for item text

--- src/components/item/itemBody.jsx
+++ src/components/item/itemBody.jsx
@@ -1,8 +1,8 @@
 function ItemBody({ category, name, price }) {
   return (
     <>
-      <h2 className="item__category">{category}</h2>
-      <h3 className="item__name">{name}</h3>
+      <p className="item__category">{category}</p>
+      <h2 className="item__name">{name}</h2>
       <p className="item__price">${price}</p>
     </>
   );
