9cf3b34e85b56c44177818f38cef74b2359cd848
Jhon Alessandro <astralgmd@gmail.com>
2024-08-01 17:26:33 -0400

    feat(Item-Button): add state after incrementation or decrementation

--- src/components/item/button.jsx
+++ src/components/item/button.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import cartIcon from "../../../public/icons/icon-add-to-cart.svg";
+import decrementIcon from "../../../public/icons/icon-decrement-quantity.svg";
+import incrementIcon from "../../../public/icons/icon-increment-quantity.svg";
 
 function Button() {
+  const [amount, setAmount] = useState(0);
+
+  let buttonClass;
+
+  !amount
+    ? (buttonClass = "item__button")
+    : (buttonClass = "item__button item__button--active");
+  function incrementAmount() {
+    setAmount(amount + 1);
+  }
+
+  function decrementAmount() {
+    setAmount(amount - 1);
+  }
+
+  if (amount === 0) {
+    return (
+      <button className={buttonClass} onClick={incrementAmount}>
+        <img src={cartIcon} alt="" />
+        <span className="item__button-text">Add to cart</span>
+      </button>
+    );
+  }
+
   return (
-    <button className="item__button">
-      <img src={cartIcon} alt="" />
-      <span className="item__button-text">Add to cart</span>
+    <button className={buttonClass}>
+      <img src={decrementIcon} alt="" onClick={decrementAmount} />
+      <span className="item__button-text">{amount}</span>
+      <img src={incrementIcon} alt="" onClick={incrementAmount} />
     </button>
   );
 }
