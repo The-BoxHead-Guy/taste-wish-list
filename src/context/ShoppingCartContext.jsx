3bc0e5a56069a4895f6cdba85017bea6764b1800
Jhon Alessandro <astralgmd@gmail.com>
2024-08-07 21:02:50 -0400

    refactor: create shoppingCartProvider to handle products state changings

---
+++ src/context/ShoppingCartContext.jsx
@@ -0,0 +1,68 @@
+import { createContext, useState, useContext } from "react";
+
+const ShoppingCartContext = createContext({});
+
+export function useShoppingCartContext() {
+  return useContext(ShoppingCartContext);
+}
+
+export function ShoppingCartProvider({ children }) {
+  /* We create the state of the cartItems in the context */
+  const [cartItems, setCartItems] = useState([]);
+
+  function getItemQuantity(id) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  }
+
+  function increaseCartQuantity(id) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) === undefined) {
+        return [...currItems, { id, quantity: 1 }];
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.uantiqty === 1) {
+        return currItems.filter((item) => item.id !== id);
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  }
+
+  function removeFromCart(id) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        cartItems,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
