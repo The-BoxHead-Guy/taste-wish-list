f56d7e1554368eff8ac4c357d97207b0f0bc2e10
Jhon Alessandro <astralgmd@gmail.com>
2024-08-08 21:00:36 -0400

    feat(cart): create static cart layout

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,3 +1,17 @@
-function Cart() {}
+function Cart() {
+  return (
+    <>
+      <div className="cart">
+        <div className="cart__title-container">
+          <h2 className="cart__title">Your Cart (0)</h2>
+        </div>
+        <div className="cart__list">
+          <img src="../../public/icons/illustration-empty-cart.svg" alt="" />
+          <p className="cart__text">Your added items will appear here</p>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Cart;
