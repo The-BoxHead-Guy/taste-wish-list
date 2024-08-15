092d25378feabf009444a49181f438f31a9b225b
Jhon Alessandro <astralgmd@gmail.com>
2024-08-14 21:20:11 -0400

    style(cart): modify images sources depending on browser dimensions

--- src/components/item/ItemImage.jsx
+++ src/components/item/ItemImage.jsx
@@ -1,6 +1,8 @@
 function ItemImage({ imgDimensions, altText, onOutline }) {
   return (
     <>
+      <source media="(min-width: 1080px)" srcSet={imgDimensions.desktop} />
+      <source media="(min-width: 768px)" srcSet={imgDimensions.tablet} />
       <img
         src={imgDimensions.mobile}
         alt={altText}
