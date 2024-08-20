8033b2894de27fb50b435128f07280c99cb293e7
Jhon Alessandro <astralgmd@gmail.com>
2024-08-19 22:36:00 -0400

    feat(overlay): disable background scrolling after order confirmation activation

--- src/components/util/Overlay.jsx
+++ src/components/util/Overlay.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 function Overlay() {
   const { toggleConfirmation } = useShoppingCartContext();
+
+  useEffect(() => {
+    const ORIGINAL_OVERFLOW = document.body.style.overflow;
+
+    if (toggleConfirmation) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.body.style.overflow = ORIGINAL_OVERFLOW;
+    };
+  }, [toggleConfirmation]);
 
   return <>{toggleConfirmation && <div className="overlay--active"></div>}</>;
 }
